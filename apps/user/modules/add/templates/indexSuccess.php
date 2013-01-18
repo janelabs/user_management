957b5a88ce64cb0f6fdc2b6823ad754129ab55d7
Sarah Jane Aristain <sahjane26@gmail.com>
2013-01-18 12:29:01 +0800

    Added a form for user's  information

--- apps/user/modules/add/templates/indexSuccess.php
+++ apps/user/modules/add/templates/indexSuccess.php
@@ -1,3 +1,13 @@
-ADD!!
+<form name="frmAdd" id="frmAdd" method="post" action="">
+
+    <label for="uname">Name</label>
+    <input type="text" id="uname" name="uname" />
+
+    <label for="bdate">Birth Date</label>
+    <input type="text" id="bdate" name="bdate" />
+
+    <label for="address">Address</label>
+    <input type="text" id="address" name="address" />
+</form>
 
 <a href="../list/index">Cancel</a>
