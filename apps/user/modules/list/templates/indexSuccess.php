5834faf5f1b5c5706e2d990794ccbc0edf2ac956
Sarah Jane Aristain <sahjane26@gmail.com>
2013-01-18 09:28:04 +0800

    Modify homepage.

--- apps/user/modules/list/templates/indexSuccess.php
+++ apps/user/modules/list/templates/indexSuccess.php
@@ -5,7 +5,41 @@
 
     <body>
         <div>
+            <button id="addUser">Add User</button>
+
             <h3>List of Users</h3>
+
+            <!--LIST OF USERS -- START -->
+            <table id="userListTbl" border='1' cellpadding="4" cellspacing="0">
+                <tr>
+                    <th>Name</th>
+                    <th>Birth Date</th>
+                    <th>Address</th>
+                    <th>Actions</th>
+                </tr>
+                <?php
+                    if(!empty($user)):
+                        ?>
+                        <tr>
+                            <td>a</td>
+                            <td>b</td>
+                            <td>c</td>
+                            <td>d</td>
+                        </tr>
+                        <?php
+                    else:
+                        ?>
+                        <tr>
+                            <td colspan="4" style="text-align: center;">
+                                No record(s) found.
+                            </td>
+                        </tr>
+                        <?php
+                    endif;
+                ?>
+            </table>
+            <!--LIST OF USERS -- END -->
+
         </div>
     </body>
 </html>
