bab8ffd0b2e4ad528c5b1bfb55e5a6ac8ec239a4
Sarah Jane Aristain <sahjane26@gmail.com>
2013-01-18 11:36:41 +0800

    Modify index view

--- apps/user/modules/list/templates/indexSuccess.php
+++ apps/user/modules/list/templates/indexSuccess.php
@@ -1,45 +1,47 @@
-<html>
-    <head>
-        <title>User Management - Home</title>
-    </head>
+<?php
+    use_javascript('jquery.js');
+    use_javascript('user.js');
+?>
+<div>
+    <button id="addUser">Add User</button>
 
-    <body>
-        <div>
-            <button id="addUser">Add User</button>
+    <h3>List of Users</h3>
 
-            <h3>List of Users</h3>
-
-            <!--LIST OF USERS -- START -->
-            <table id="userListTbl" border='1' cellpadding="4" cellspacing="0">
+    <!--LIST OF USERS -- START -->
+    <table id="userListTbl" border='1' cellpadding="4" cellspacing="0">
+        <tr>
+            <th>Name</th>
+            <th>Birth Date</th>
+            <th>Address</th>
+            <th>Actions</th>
+        </tr>
+        <?php
+            if(!empty($user)):
+                ?>
                 <tr>
-                    <th>Name</th>
-                    <th>Birth Date</th>
-                    <th>Address</th>
-                    <th>Actions</th>
+                    <td>a</td>
+                    <td>b</td>
+                    <td>c</td>
+                    <td>d</td>
                 </tr>
                 <?php
-                    if(!empty($user)):
-                        ?>
-                        <tr>
-                            <td>a</td>
-                            <td>b</td>
-                            <td>c</td>
-                            <td>d</td>
-                        </tr>
-                        <?php
-                    else:
-                        ?>
-                        <tr>
-                            <td colspan="4" style="text-align: center;">
-                                No record(s) found.
-                            </td>
-                        </tr>
-                        <?php
-                    endif;
+            else:
                 ?>
-            </table>
-            <!--LIST OF USERS -- END -->
+                <tr>
+                    <td colspan="4" style="text-align: center;">
+                        No record(s) found.
+                    </td>
+                </tr>
+                <?php
+            endif;
+        ?>
+    </table>
+    <!--LIST OF USERS -- END -->
+</div>
 
-        </div>
-    </body>
-</html>
+<!-- SCRIPTS -->
+<script type="text/javascript">
+    $(function(){
+        User.initView();
+    });
+</script>
