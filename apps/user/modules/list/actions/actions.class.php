edfbfea7950f849ea67f49658de700c2edac0e98
Sarah Jane Aristain <sahjane26@gmail.com>
2013-01-21 08:46:45 +0800

    Modify comment

--- apps/user/modules/list/actions/actions.class.php
+++ apps/user/modules/list/actions/actions.class.php
@@ -5,8 +5,7 @@
  *
  * @package    USER_MANAGEMENT
  * @subpackage list
- * @author     Your name here
- * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ * @author     Sarah Aristain
  */
 class listActions extends sfActions
 {
